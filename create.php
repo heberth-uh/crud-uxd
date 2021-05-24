60057fcdb51f654f57f819f25a512be1605a5c81
David Dzul <davidf.dzul@gmail.com>
2021-05-24 16:32:35 -0500

    create

--- create.php
+++ create.php
@@ -29,7 +29,7 @@ $con->close();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+    <link rel="sgit tylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
     <title>tabla de proyectos</title>
 </head>
 
